d09ea7536befa6435e17783bcd2e0adf7bc7f5ba
ShakerCode <goldenlele10@gmail.com>
2021-03-02 22:55:41 -0500

    update gitignore again

--- static_build/js/bands.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    var proximity = document.getElementById("proximity").value.trim();
-    if(isNaN(proximity) || /^ *$/.test(proximity)) {
-        console.log('');
-        alert("Enter data correctly");
-        return false
-    }
-}
